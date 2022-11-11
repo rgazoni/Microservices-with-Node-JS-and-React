fb1db928f67309af7941106c094e58d9e655094b
Ramon Gazoni Lacerda <ramongazonil@gmail.com>
2022-11-11 15:03:36 -0300

    Updating React application to suit new query microservices and solving many requests problems

--- 02-mini-microservice-blog/blog/client/src/PostList.js
+++ 02-mini-microservice-blog/blog/client/src/PostList.js
@@ -12,8 +12,7 @@ const PostList = () => {
     const fetchPosts = async () => {
         //Always when we make a request with axios, we get back an response object
         //that contains our data in a data property
-        const res = await axios.get("http://localhost:4000/posts");
-        
+        const res = await axios.get("http://localhost:4002/posts");
         setPosts(res.data);
     };
 
@@ -38,7 +37,7 @@ const PostList = () => {
         >
             <div className='card-body'>
                 <h3>{post.title}</h3>
-                <CommentList postId={post.id} />
+                <CommentList comments={post.comments} />
                 <CommentCreate postId={post.id} />
             </div>
         </div>
