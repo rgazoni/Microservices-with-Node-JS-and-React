1819fc3b0bd5272e5bb8a5abfb8bf61246b1fdef
Ramon Gazoni <ramongazonil@gmail.com>
2022-11-28 10:22:22 -0300

    Infra with K8s and docker being implemented

--- 02-mini-microservice-blog/blog/posts/index.js
+++ 02-mini-microservice-blog/blog/posts/index.js
@@ -26,7 +26,7 @@ app.post('/posts', async (req, res) => {
         title
     }
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: {
             id,
@@ -44,5 +44,5 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4000, () => {
-    console.log('Listening on port 4000');
+    console.log("Listening on port 4000");
 });
